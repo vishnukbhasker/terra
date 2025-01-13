fc97ad328141742f86f3a1c9231fe43dd5ad4006
vishnukbhasker <107983290+vishnukbhasker@users.noreply.github.com>
2025-01-13 23:58:12 +0530

    main.tf
    last

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami           = "ami-075449515af5df0d1" 
   instance_type = "t3.micro"  
-  key_name      = "mykey"
+  key_name      = "adminkey"
 
   tags = {
     Name = "Jenkins"
