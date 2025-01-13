c475f24336594a6cf613fdd940d28fe570605e4d
vishnukbhasker <107983290+vishnukbhasker@users.noreply.github.com>
2025-01-13 21:11:03 +0530

    main.tf
    second commit

--- main.tf
+++ main.tf
@@ -2,8 +2,7 @@ provider "aws" {
   region = "eu-north-1"
 }
 
-module "ec2_instance" {
-  source = "./modules/ec2_instance"
+resource "ec2_instance" "example" {
   ami_value = "ami-075449515af5df0d1" # replace this
   instance_type_value = "t3.micro"
   subnet_id_value = "subnet-0df037d5b98ca8043". # replace this
