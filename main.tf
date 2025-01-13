898e7199285618b80dd886a799764a6039834794
vishnukbhasker <107983290+vishnukbhasker@users.noreply.github.com>
2025-01-13 20:57:33 +0530

    main.tf
    first commit

--- main.tf
+++ main.tf
@@ -1,1 +1,10 @@
+provider "aws" {
+  region = "eu-north-1"
+}
 
+module "ec2_instance" {
+  source = "./modules/ec2_instance"
+  ami_value = "ami-075449515af5df0d1" # replace this
+  instance_type_value = "t3.micro"
+  subnet_id_value = "subnet-0df037d5b98ca8043". # replace this
+}
